a1587168fbc213c6a85b2f7491e5ef5b9ee99203
Luke Brady <lzbrady496@gmail.com>
2019-08-12 21:10:02 -0400

    Changed favicon

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         color: `#fff3e1`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
