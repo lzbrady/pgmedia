5358ba0cb4383b58ecb476bfac001fa998aabd7a
Luke Brady <lzbrady496@gmail.com>
2019-11-21 22:01:04 -0500

    updated 404 page

--- app/src/pages/404.js
+++ app/src/pages/404.js
@@ -3,8 +3,18 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-function NotFoundPage() {
-  window.location = "/"
+class NotFoundPage extends React.Component {
+  componentDidMount() {
+    window.location = "/"
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Page Not Found :/</p>
+      </div>
+    )
+  }
 }
 
 export default NotFoundPage
