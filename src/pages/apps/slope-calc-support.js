18596921b3b50ae4fda9c50a6beebe1b6f48d973
Luke Brady <lzbrady496@gmail.com>
2021-05-09 20:01:27 -0400

    Added slope calc support url

---
+++ src/pages/apps/slope-calc-support.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SlopeCalcSupport({}) {
+  return (
+    <div>
+      <p>For any questions, comments, feedback, or need for support, please email</p>
+      <a href="mailto:luke@pgmediasolutions.com?subject=Slope and Triangle Calculator Support">luke@pgmediasolutions.com</a>
+    </div>
+  );
+}
